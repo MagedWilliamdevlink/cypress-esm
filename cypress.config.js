409d864dc7d76e129ad1bb5cbe9ea22241bd18f7
MagedWilliamdevlink <109914773+MagedWilliamdevlink@users.noreply.github.com>
2024-04-29 21:36:59 +0300

    Update cypress.config.js
    uncomment to see the issue

--- cypress.config.js
+++ cypress.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
           viteConfig,
           {
             plugins: [
-              // CypressEsm(),
+              // CypressEsm(), // uncomment to see the issue
             ]
           }
         )
